2a10117ecf4e3b495ee1f6587eccd1af55ed47f2
Tks23-hub <forcodin23@gmail.com>
2024-09-30 14:15:43 +0300

    הוספתי const path = require('path');

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const port = 7575;
 const app = express();
+const path = require('path');
 app.use(express.json());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.set("view engine", "ejs");
-app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const fe = require('./FE_R');
-const {join} = require("node:path");
 app.use('/', fe);
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
