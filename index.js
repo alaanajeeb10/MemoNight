68911704c2f0b3bff2f736d26dfc5c6461addc5f
Tks23-hub <forcodin23@gmail.com>
2024-09-30 13:45:18 +0300

    הוספת רוטרים ל-FE_R להצגת עמוד הנקודות

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.set("view engine", "ejs");
 
+
+const fe = require('./FE_R');
+app.use('/', fe);
+
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port http://localhost:${port}`);
 });
