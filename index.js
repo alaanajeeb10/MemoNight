a28d1738d5eab08c4046be73153095d108c7fc56
Tks23-hub <forcodin23@gmail.com>
2024-09-30 14:10:56 +0300

    הוספתי תיקיית public וקובץ CSS בשם pointsstyle לעיצוב הדף

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.set("view engine", "ejs");
-
+app.use(express.static(join(__dirname, 'public')));
 
 const fe = require('./FE_R');
+const {join} = require("node:path");
 app.use('/', fe);
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
